6054dd3da61ebe5782738c7552493f638e3d14da
mytreo <mytreogm@gmail.com>
2016-02-26 14:07:12 +0200

    send in another thread

--- src/main/java/ua/mytreo/java/jwpopup/sys/mailslot/MailSlotSender.java
+++ src/main/java/ua/mytreo/java/jwpopup/sys/mailslot/MailSlotSender.java
@@ -25,7 +25,7 @@ public class MailSlotSender {
         char a = '\0'; //EndOfString NULL
         char b = '\13'; //enter CR
         char c = '\10'; //NewString LF
-        boolean result = false;
+
         String fullPath = "\\\\" + to + "\\mailslot\\" + slotPath;
         //  '\0' = #0
         // msgFormat =  "Sender+#0+Reciever+#0^ ololo message"+ +#13#10 podpis +#13#10 deviz"
@@ -45,37 +45,57 @@ public class MailSlotSender {
             messages[0] = "^" + textMessage;
         }
 
-        for (String message : messages) {
-            String msg = prefix + message;
-            System.out.println("Try to send message " + msg + " into " + fullPath);
+        //send messages in another thread
+        new Thread(new SenderHelper(messages,prefix, fullPath)).start();
 
-            IntByReference written = new IntByReference();
-            int hFile = k32lib.CreateFile(fullPath, JNAKernel32.GENERIC_WRITE, JNAKernel32.FILE_SHARE_READ, 0, JNAKernel32.OPEN_EXISTING, JNAKernel32.FILE_ATTRIBUTE_NORMAL, 0);
-            try {
-                if (hFile != JNAKernel32.INVALID_HANDLE_VALUE) {
+    }
 
-                    ByteBuffer bb = Charset.forName("cp866").encode(msg);
-                    k32lib.WriteFile(hFile, bb, bb.array().length, written, 0);
-                    if (!((msg.length()) == written.getValue())) {
-                        throw new Exception("something with msg length");
+    private class SenderHelper implements Runnable{
+        private String[] messages;
+        private String prefix;
+        private String fullPath;
+        private boolean result = false;
+
+        SenderHelper(String[] messages,String prefix,String fullPath){
+            this.messages=messages;
+            this.prefix=prefix;
+            this.fullPath=fullPath;
+
+        }
+        @Override
+        public void run() {
+            for (String message : messages) {
+                String msg = prefix + message;
+                System.out.println("Try to send message " + msg + " into " + fullPath);
+
+                IntByReference written = new IntByReference();
+                int hFile = k32lib.CreateFile(fullPath, JNAKernel32.GENERIC_WRITE, JNAKernel32.FILE_SHARE_READ, 0, JNAKernel32.OPEN_EXISTING, JNAKernel32.FILE_ATTRIBUTE_NORMAL, 0);
+                try {
+                    if (hFile != JNAKernel32.INVALID_HANDLE_VALUE) {
+
+                        ByteBuffer bb = Charset.forName("cp866").encode(msg);
+                        k32lib.WriteFile(hFile, bb, bb.array().length, written, 0);
+                        if (!((msg.length()) == written.getValue())) {
+                            throw new Exception("something with msg length");
+                        }
+
+                        System.out.println("Msg dostavko " + Integer.toString(written.getValue()) + " octets");
+                        result = true;
+                    } else {
+                        throw new Exception("ohShit invalid slot");
                     }
 
-                    System.out.println("Msg dostavko " + Integer.toString(written.getValue()) + " octets");
-                    result = true;
-                } else {
-                    throw new Exception("ohShit invalid slot");
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                } finally {
+                    k32lib.CloseHandle(hFile);
                 }
 
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            } finally {
-                k32lib.CloseHandle(hFile);
-            }
-
-            if (result) {
-                System.out.println("Otpravleno");
-            } else {
-                System.out.println("Ne otpravleno");
+                if (result) {
+                    System.out.println("Otpravleno");
+                } else {
+                    System.out.println("Ne otpravleno");
+                }
             }
         }
     }
